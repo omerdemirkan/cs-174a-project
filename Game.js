44694bca6027a69ade1524917ae6c3d6c7769ea8
omerdemirkan <omerfarukpiano@gmail.com>
2023-02-24 11:51:06 -0800

    Add pacman movement logic (tested)

--- Game.js
+++ Game.js
@@ -5,31 +5,27 @@ class Game {
     ghostBlocksPerSecondSpeed = 2,
   } = {}) {
     this._GAME_STATE_REFRESH_MS = 1000 * (1 / gameStateRefreshRate);
-    const pacmanBlocksPerMillisecond = pacmanBlocksPerSecondSpeed * 1000;
+    const pacmanBlocksPerMillisecond = pacmanBlocksPerSecondSpeed / 1000;
     this._PACMAN_MOVEMENT_PER_REFRESH =
-      pacmanBlocksPerMillisecond / this._GAME_STATE_REFRESH_MS;
+      pacmanBlocksPerMillisecond * this._GAME_STATE_REFRESH_MS;
 
     this._GHOST_MOVEMENT_PER_REFRESH = ghostBlocksPerSecondSpeed / 1000;
 
     this._interval = null;
 
     this._pacman = {
-      x: 1,
-      y: 1,
-      direction: DIRECTIONS.NONE,
+      position: {
+        i: 1,
+        j: 1,
+        z: 0,
+      },
+      movementDirection: DIRECTIONS.NONE,
       intendedDirection: DIRECTIONS.NONE,
     };
 
     // Creating a copy to avoid polluting it for a given game
     this._matrix = INITIAL_MATRIX.map((row) => [...row]);
   }
-
-  _handleRefresh = () => {
-    const isPacmanIntendingToMove =
-      this._pacman.intendedDirection !== DIRECTIONS.NONE;
-    if (isPacmanIntendingToMove) {
-    }
-  };
 
   startGame = () => {
     if (!this._interval) {
@@ -45,14 +41,82 @@ class Game {
     this.interval = null;
   };
 
+  _handleRefresh = () => {
+    // HANDLE PACMAN MOVEMENT
+
+    const pacmanNextPosition = {
+      i:
+        this._pacman.position.i +
+        this._PACMAN_MOVEMENT_PER_REFRESH * this._pacman.movementDirection.i,
+      j:
+        this._pacman.position.j +
+        this._PACMAN_MOVEMENT_PER_REFRESH * this._pacman.movementDirection.j,
+      z: this._pacman.position.z,
+    };
+
+    const isPacmanEnteringNewBlock =
+      Math.floor(this._pacman.position.i) !==
+        Math.floor(pacmanNextPosition.i) ||
+      Math.floor(this._pacman.position.j) !==
+        Math.floor(pacmanNextPosition.j) ||
+      Math.ceil(this._pacman.position.i) !== Math.ceil(pacmanNextPosition.i) ||
+      Math.ceil(this._pacman.position.j) !== Math.ceil(pacmanNextPosition.j);
+
+    if (isPacmanEnteringNewBlock) {
+      const discreteI = Math.round(this._pacman.position.i);
+      const discreteJ = Math.round(this._pacman.position.j);
+
+      const isIntendingToTurn =
+        this._pacman.movementDirection !== this._pacman.intendedDirection;
+
+      const isMovingInIntendedDirectionPossible =
+        this._matrix[discreteI + this._pacman.intendedDirection.i]?.[
+          discreteJ + this._pacman.intendedDirection.j
+        ] !== OBJECTS.BARRIER;
+
+      const isTurning =
+        isIntendingToTurn && isMovingInIntendedDirectionPossible;
+
+      const isPacmanWalkingIntoAWall =
+        this._matrix[Math.floor(pacmanNextPosition.i)]?.[
+          Math.floor(pacmanNextPosition.j)
+        ] === OBJECTS.BARRIER ||
+        this._matrix[Math.ceil(pacmanNextPosition.i)]?.[
+          Math.ceil(pacmanNextPosition.j)
+        ] === OBJECTS.BARRIER;
+
+      if (isTurning) {
+        this._pacman.movementDirection = this._pacman.intendedDirection;
+        pacmanNextPosition.i = discreteI;
+        pacmanNextPosition.j = discreteJ;
+      } else if (isPacmanWalkingIntoAWall) {
+        // Places pacman up against the wall.
+        // This is assuming a short game tick!
+        pacmanNextPosition.i = discreteI;
+        pacmanNextPosition.j = discreteJ;
+      }
+    }
+
+    this._pacman.position = pacmanNextPosition;
+  };
+
+  _getXfromJ = (j) => {
+    return j;
+  };
+
+  _getYfromI = (i) => {
+    return this._matrix.length - 1 - i;
+  };
+
   getBarriers = () => {
     const barriers = [];
     this._matrix.forEach((row, i) => {
       row.forEach((item, j) => {
         if (item === OBJECTS.BARRIER) {
           barriers.push({
-            x: j,
-            y: i,
+            x: this._getXfromJ(j),
+            y: this._getYfromI(i),
+            z: 0,
             hasBarrierAbove: this._matrix[i - 1]?.[j] === OBJECTS.BARRIER,
             hasBarrierBelow: this._matrix[i + 1]?.[j] === OBJECTS.BARRIER,
             hasBarrierLeft: this._matrix[i]?.[j - 1] === OBJECTS.BARRIER,
@@ -64,6 +128,26 @@ class Game {
 
     return barriers;
   };
+
+  handleChangeIntendedDirection = (direction) => {
+    if (!Object.values(DIRECTIONS).includes(direction)) {
+      throw new Error(
+        "INVALID DIRECTION! Pass in DIRECTION.{direction} into handleChangeIntendedDirection."
+      );
+    }
+
+    this._pacman.intendedDirection = direction;
+    const isIntendedDirectionOppositeToMovementDirection =
+      this._pacman.movementDirection.i === -direction.i &&
+      this._pacman.movementDirection.j === -direction.j;
+
+    if (
+      isIntendedDirectionOppositeToMovementDirection ||
+      this._pacman.movementDirection === DIRECTIONS.NONE
+    ) {
+      this._pacman.movementDirection = direction;
+    }
+  };
 }
 
 const OBJECTS = Object.freeze({
@@ -74,11 +158,11 @@ const OBJECTS = Object.freeze({
 });
 
 const DIRECTIONS = Object.freeze({
-  UP: 0,
-  DOWN: 1,
-  LEFT: 2,
-  RIGHT: 3,
-  NONE: 4, // at the beginning, pacman doesn't have a direction.
+  UP: Object.freeze({ j: 0, i: -1 }),
+  DOWN: Object.freeze({ j: 0, i: 1 }),
+  LEFT: Object.freeze({ j: -1, i: 0 }),
+  RIGHT: Object.freeze({ j: 1, i: 0 }),
+  NONE: Object.freeze({ j: 0, i: 0 }), // at the beginning, pacman doesn't have a direction.
 });
 
 const INITIAL_MATRIX = [
