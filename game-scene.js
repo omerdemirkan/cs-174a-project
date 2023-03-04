a236605f5009da47e43d975a7cfa9465598192a2
William Li <williamzli@gmail.com>
2023-03-03 18:53:45 -0800

    ghost light effect

--- game-scene.js
+++ game-scene.js
@@ -17,7 +17,7 @@ const {
   Material,
   Scene,
 } = tiny;
-const colors = {blue: hex_color("#00008B"), white: hex_color("#FFFFFF")}
+const colors = { blue: hex_color("#00008B"), white: hex_color("#FFFFFF") };
 export class GameScene extends Scene {
   constructor() {
     // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
@@ -31,9 +31,19 @@ export class GameScene extends Scene {
 
     // *** Materials
     this.materials = {
-      test: new Material(new defs.Phong_Shader(), {
-        ambient: 0.4,
-        diffusivity: 0.6,
+      wall_mat: new Material(new defs.Phong_Shader(5), {
+        ambient: 0.25,
+        diffusivity: 0.5,
+        color: hex_color("#ffffff"),
+      }),
+      pacman_mat: new Material(new defs.Phong_Shader(5), {
+        ambient: 0.5,
+        diffusivity: 0.5,
+        color: hex_color("#ffffff"),
+      }),
+      ghost_mat: new Material(new defs.Phong_Shader(5), {
+        ambient: 1,
+        diffusivity: 0,
         color: hex_color("#ffffff"),
       }),
     };
@@ -75,31 +85,36 @@ export class GameScene extends Scene {
 
   color_algo(hex, i, j) {
     let to_paint = colors.blue;
-    if (hex[i+1] != undefined && hex[i-1] != undefined) {
-      if(hex[i+1][j] == 0 || hex[i-1][j] == 0 && !(i == 0 && j == 0)) {
+    if (hex[i + 1] != undefined && hex[i - 1] != undefined) {
+      if (hex[i + 1][j] == 0 || (hex[i - 1][j] == 0 && !(i == 0 && j == 0))) {
         to_paint = colors.white;
       }
     }
-    if(hex[i][j+1] == 0 || hex[i][j-1] == 0) {
+    if (hex[i][j + 1] == 0 || hex[i][j - 1] == 0) {
       to_paint = colors.white;
     }
-    if(i == 1 && j == 1) {
-      if((hex[i][j+1] && hex[i+1][j] && !hex[i+1][j+1]) || (hex[i][j-1] && hex[i+1][j] && !hex[i-1][j+1])) {
+    if (i == 1 && j == 1) {
+      if (
+        (hex[i][j + 1] && hex[i + 1][j] && !hex[i + 1][j + 1]) ||
+        (hex[i][j - 1] && hex[i + 1][j] && !hex[i - 1][j + 1])
+      ) {
         to_paint = colors.white;
       }
     }
-    return to_paint
+    return to_paint;
   }
 
   wall_builder(context, program_state, barrier) {
-    let x = (barrier.x - 1/3);
-    let y = (barrier.y - 1/3);
+    let x = barrier.x - 1 / 3;
+    let y = barrier.y - 1 / 3;
     let z = barrier.z;
     let x_mod = 0.0;
-    let hex = [[0,0,0],
-               [0,1,0],
-               [0,0,0]];
-    
+    let hex = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+
     if (barrier.hasBarrierAbove) {
       hex[1][2] = 1;
     }
@@ -112,33 +127,51 @@ export class GameScene extends Scene {
     if (barrier.hasBarrierLeft) {
       hex[0][1] = 1;
     }
-    if (barrier.hasBarrierAboveLeft && barrier.hasBarrierAbove && barrier.hasBarrierLeft) {
+    if (
+      barrier.hasBarrierAboveLeft &&
+      barrier.hasBarrierAbove &&
+      barrier.hasBarrierLeft
+    ) {
       hex[0][2] = 1;
     }
-    if (barrier.hasBarrierBelowLeft && barrier.hasBarrierBelow && barrier.hasBarrierLeft) {
+    if (
+      barrier.hasBarrierBelowLeft &&
+      barrier.hasBarrierBelow &&
+      barrier.hasBarrierLeft
+    ) {
       hex[0][0] = 1;
     }
-    if (barrier.hasBarrierAboveRight && barrier.hasBarrierAbove && barrier.hasBarrierRight) {
+    if (
+      barrier.hasBarrierAboveRight &&
+      barrier.hasBarrierAbove &&
+      barrier.hasBarrierRight
+    ) {
       hex[2][2] = 1;
     }
-    if (barrier.hasBarrierBelowRight && barrier.hasBarrierBelow && barrier.hasBarrierRight) {
+    if (
+      barrier.hasBarrierBelowRight &&
+      barrier.hasBarrierBelow &&
+      barrier.hasBarrierRight
+    ) {
       hex[2][0] = 1;
     }
 
     for (const i of Array(3).keys()) {
       let y_mod = 0.0;
-      for(const j of Array(3).keys()) {
+      for (const j of Array(3).keys()) {
         if (hex[i][j]) {
           this.shapes.box.draw(
             context,
             program_state,
-            Mat4.translation((x + x_mod) * 2, (y + y_mod) * 2, z).times(Mat4.scale(1/3, 1/3, 1)),
-            this.materials.test.override({color: this.color_algo(hex, i, j)})
+            Mat4.translation((x + x_mod) * 2, (y + y_mod) * 2, z).times(
+              Mat4.scale(1 / 3, 1 / 3, 1)
+            ),
+            this.materials.wall_mat.override({ color: this.color_algo(hex, i, j) })
           );
         }
-        y_mod += 1/3;
+        y_mod += 1 / 3;
       }
-      x_mod += 1/3;
+      x_mod += 1 / 3;
     }
   }
 
@@ -160,17 +193,33 @@ export class GameScene extends Scene {
       0.1,
       1000
     );
-
-    const light_position = vec4(0, 0, 50, 1);
+    const playerState = this.game.getPacman();
+    const yellow = hex_color("#fac91a");
+    const colors = ["#FF0000", "#FFC0CB", "#0000FF", "#FFA500"];
+    let player_light = vec4(
+      playerState.position.x * 2,
+      playerState.position.y * 2,
+      playerState.position.z * 2,
+      1
+    );
     // The parameters of the Light are: position, color, size
-    program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+    program_state.lights = [new Light(player_light, yellow, 50)];
+    this.game.getGhosts().forEach((ghost, index) => {
+      let ghost_light = vec4(
+        ghost.position.x * 2,
+        ghost.position.y * 2,
+        ghost.position.z * 2,
+        1
+      );
+      program_state.lights.push(
+        new Light(ghost_light, hex_color(colors[index]), 50)
+      );
+    });
+    console.log(program_state.lights);
 
     // TODO:  Fill in matrix operations and drawing code to draw the solar system scene (Requirements 3 and 4)
     const t = program_state.animation_time / 1000,
       dt = program_state.animation_delta_time / 1000;
-    const yellow = hex_color("#fac91a");
-
-    const playerState = this.game.getPacman();
 
     this.shapes.box.draw(
       context,
@@ -180,10 +229,9 @@ export class GameScene extends Scene {
         playerState.position.y * 2,
         playerState.position.z * 2
       ).times(Mat4.rotation(playerState.rotationAngleInRadians, 0, 0, 1)),
-      this.materials.test.override({ color: yellow })
+      this.materials.pacman_mat.override({ color: yellow })
     );
 
-    const colors = ["#FF0000", "#FFC0CB", "#0000FF", "#FFA500"]
     this.game.getGhosts().forEach((ghost, index) => {
       this.shapes.box.draw(
         context,
@@ -193,7 +241,7 @@ export class GameScene extends Scene {
           ghost.position.y * 2,
           ghost.position.z * 2
         ).times(Mat4.rotation(ghost.rotationAngleInRadians, 0, 0, 1)),
-        this.materials.test.override({ color: hex_color(colors[index]) })
+        this.materials.ghost_mat.override({ color: hex_color(colors[index]) })
       );
     });
 
