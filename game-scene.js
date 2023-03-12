4e3892e33bacf7986c9e623944cc1c52a3d4f841
omerdemirkan <omerfarukpiano@gmail.com>
2023-03-12 11:28:42 -0700

    Make inner wall blocks float independently.

--- game-scene.js
+++ game-scene.js
@@ -61,33 +61,60 @@ export class GameScene extends Scene {
       }),
 
       center_rail: new Material(new defs.Fake_Bump_Map(5), {
-          ambient: 0.3, diffusivity: 1, specularity: 1, texture: new Texture("assets/cubeshade.png", "LINEAR_MIPMAP_LINEAR")
+        ambient: 0.3,
+        diffusivity: 1,
+        specularity: 1,
+        texture: new Texture("assets/cubeshade.png", "LINEAR_MIPMAP_LINEAR"),
       }),
 
       monolith: new Material(new defs.Fake_Bump_Map(5), {
-        ambient: 0.3, diffusivity: 1, specularity: 1, texture: new Texture("assets/cubeshade_circ.png", "LINEAR_MIPMAP_LINEAR")
+        ambient: 0.3,
+        diffusivity: 1,
+        specularity: 1,
+        texture: new Texture(
+          "assets/cubeshade_circ.png",
+          "LINEAR_MIPMAP_LINEAR"
+        ),
       }),
 
       center_rail_rot: new Material(new defs.Fake_Bump_Map(5), {
-        ambient: 0.3, diffusivity: 1, specularity: 1, texture: new Texture("assets/cubeshade_rot.png", "LINEAR_MIPMAP_LINEAR")
+        ambient: 0.3,
+        diffusivity: 1,
+        specularity: 1,
+        texture: new Texture(
+          "assets/cubeshade_rot.png",
+          "LINEAR_MIPMAP_LINEAR"
+        ),
       }),
-    
+
       wall_floor: new Material(new defs.Fake_Bump_Map(5), {
-        ambient: 0.1, diffusivity: 0.5, specularity: 0.3, texture: new Texture("assets/scaled_walls.png", "LINEAR_MIPMAP_LINEAR")
+        ambient: 0.1,
+        diffusivity: 0.5,
+        specularity: 0.3,
+        texture: new Texture("assets/scaled_walls.png", "LINEAR_MIPMAP_LINEAR"),
       }),
 
       trail: new Material(new defs.Fake_Bump_Map(5), {
-        ambient: 0, diffusivity: 1, specularity: 1, texture: new Texture("assets/glow_trail.png", "LINEAR_MIPMAP_LINEAR"),
-        color: color(1,1,1,1)
+        ambient: 0,
+        diffusivity: 1,
+        specularity: 1,
+        texture: new Texture("assets/glow_trail.png", "LINEAR_MIPMAP_LINEAR"),
+        color: color(1, 1, 1, 1),
       }),
 
       trail_floors: new Material(new Textured_Phong(5), {
-        ambient: 0.25, diffusivity:0, specularity:0, texture: new Texture("assets/floors.png", "LINEAR_MIPMAP_LINEAR")
+        ambient: 0.25,
+        diffusivity: 0,
+        specularity: 0,
+        texture: new Texture("assets/floors.png", "LINEAR_MIPMAP_LINEAR"),
       }),
 
       white: new Material(new Textured_Phong(5), {
-        ambient: 0.2, diffusivity: 0.5, specularity: 0, texture: new Texture("assets/whitemap.png")
-      })
+        ambient: 0.2,
+        diffusivity: 0.5,
+        specularity: 0,
+        texture: new Texture("assets/whitemap.png"),
+      }),
     };
 
     this.initial_camera_location = Mat4.look_at(
@@ -153,7 +180,7 @@ export class GameScene extends Scene {
 
   wall_builder(context, program_state, barrier, index) {
     const startMs = Date.now();
-    let t = program_state.animation_time / 1000
+    let t = program_state.animation_time / 1000;
     let x = barrier.x - 1 / 3;
     let y = barrier.y - 1 / 3;
     let z = barrier.z;
@@ -163,20 +190,21 @@ export class GameScene extends Scene {
       for (const j of Array(3).keys()) {
         if (this.barrierCubes[index][i][j]) {
           let mat_shad = this.materials.monolith.override({
-            color: color(Math.sin(t + Math.PI), Math.cos(t), Math.sin(t), 1)
-          })
+            color: color(Math.sin(t + Math.PI), Math.cos(t), Math.sin(t), 1),
+          });
           if (this.colorCube[index][i][j] == colors.white) {
-            mat_shad = this.materials.center_rail.override({
-            })
-            if (this.barrierCubes[index][i][0] && 
-                this.barrierCubes[index][i][1] &&
-                this.barrierCubes[index][i][2]) {
-                  mat_shad = this.materials.center_rail_rot
+            mat_shad = this.materials.center_rail.override({});
+            if (
+              this.barrierCubes[index][i][0] &&
+              this.barrierCubes[index][i][1] &&
+              this.barrierCubes[index][i][2]
+            ) {
+              mat_shad = this.materials.center_rail_rot;
             }
 
-            if (i == 1 && j == 0 || j == 2) {
-              if(this.colorCube[index][i][1] == colors.white) {
-                mat_shad = this.materials.center_rail_rot
+            if ((i == 1 && j == 0) || j == 2) {
+              if (this.colorCube[index][i][1] == colors.white) {
+                mat_shad = this.materials.center_rail_rot;
               }
             }
           }
@@ -184,7 +212,13 @@ export class GameScene extends Scene {
             context,
             program_state,
             Mat4.translation((x + x_mod) * 2, (y + y_mod) * 2, z).times(
-              Mat4.scale(1 / 3, 1 / 3, this.colorCube[index][i][j] == colors.white ? 1 : 1 + Math.abs(Math.sin(t + x + y)))
+              Mat4.scale(
+                1 / 3,
+                1 / 3,
+                this.colorCube[index][i][j] == colors.white
+                  ? 1
+                  : 1 + Math.abs(Math.sin(t + (x + x_mod) + (y + y_mod)))
+              )
             ),
             mat_shad
           );
@@ -246,11 +280,11 @@ export class GameScene extends Scene {
       playerState.position.z * 2,
       1
     );
-    let playerIllum = null
+    let playerIllum = null;
     if (playerState.isOnSmoke) {
-      playerIllum = new Light(player_light, yellow, 500)
+      playerIllum = new Light(player_light, yellow, 500);
     } else {
-      playerIllum = new Light(player_light, yellow, 50)
+      playerIllum = new Light(player_light, yellow, 50);
     }
     // The parameters of the Light are: position, color, size
     program_state.lights = [playerIllum];
@@ -285,8 +319,13 @@ export class GameScene extends Scene {
           playerState.position.y * 2,
           playerState.position.z * 2
         ).times(Mat4.rotation(1, 1, 0, 0)),
-        this.materials.pac0.override({ambient: Date.now() - playerState.lastPowerUpEatenMillisecond <
-          this.game._PACMAN_ON_SMOKE_DURATION_MS - 1500 ? 1 :  0.75})
+        this.materials.pac0.override({
+          ambient:
+            Date.now() - playerState.lastPowerUpEatenMillisecond <
+            this.game._PACMAN_ON_SMOKE_DURATION_MS - 1500
+              ? 1
+              : 0.75,
+        })
       );
     } else {
       this.shapes.pacman.draw(
@@ -300,7 +339,9 @@ export class GameScene extends Scene {
           .times(Mat4.rotation(playerState.rotationAngleInRadians, 0, 0, 1))
           .times(Mat4.rotation(Math.PI / 2, 0, 0, -1))
           .times(Mat4.rotation(Math.PI / 2, 1, 0, 0)),
-        this.materials.pac1.override({ambient: playerState.isOnSmoke ? 1 :  0.75})
+        this.materials.pac1.override({
+          ambient: playerState.isOnSmoke ? 1 : 0.75,
+        })
       );
     }
 
@@ -341,25 +382,27 @@ export class GameScene extends Scene {
     this.shapes.box.draw(
       context,
       program_state,
-      Mat4.translation(8 * 2, 8 * 2, -1.3).times(Mat4.scale(17,17,0.001)),
-      this.materials.white.override({color: color(0.75, 0.75, 0.75, 1)})
+      Mat4.translation(8 * 2, 8 * 2, -1.3).times(Mat4.scale(17, 17, 0.001)),
+      this.materials.white.override({ color: color(0.75, 0.75, 0.75, 1) })
     );
     this.shapes.box.draw(
       context,
       program_state,
-      Mat4.translation(8 * 2, 8 * 2, -1.1).times(Mat4.scale(17,17,0.001)),
-      this.materials.trail_floors.override({ color: color(Math.sin(t + Math.PI), Math.cos(t), Math.sin(t), 0.5)})
+      Mat4.translation(8 * 2, 8 * 2, -1.1).times(Mat4.scale(17, 17, 0.001)),
+      this.materials.trail_floors.override({
+        color: color(Math.sin(t + Math.PI), Math.cos(t), Math.sin(t), 0.5),
+      })
     );
     this.shapes.box.draw(
       context,
       program_state,
-      Mat4.translation(8 * 2, 8 * 2, -1).times(Mat4.scale(17,17,0.001)),
+      Mat4.translation(8 * 2, 8 * 2, -1).times(Mat4.scale(17, 17, 0.001)),
       this.materials.wall_floor
     );
     this.shapes.box.draw(
       context,
       program_state,
-      Mat4.translation(8 * 2, 8 * 2, -1).times(Mat4.scale(17,17,0.001)),
+      Mat4.translation(8 * 2, 8 * 2, -1).times(Mat4.scale(17, 17, 0.001)),
       this.materials.trail
     );
 
@@ -370,10 +413,11 @@ export class GameScene extends Scene {
         Mat4.translation(powerUp.x * 2, powerUp.y * 2, powerUp.z * 2).times(
           Mat4.scale(0.5, 0.5, 0.5)
         ),
-        this.materials.ghost_mat.override({color: color(0.5 * Math.sin(t) + 1, 0.5 * Math.sin(t) + 1, 0, 1)})
+        this.materials.ghost_mat.override({
+          color: color(0.5 * Math.sin(t) + 1, 0.5 * Math.sin(t) + 1, 0, 1),
+        })
       );
     });
- 
   }
 }
 
@@ -473,9 +517,9 @@ function color_algo(hex, i, j) {
 
 function is_between(lower, upper, n) {
   if (n < lower) {
-    return lower
+    return lower;
   } else if (n > upper) {
-    return upper
+    return upper;
   }
-  return n
+  return n;
 }
